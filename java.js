aae99e215505770025238a08bf77a6517f137acb
Anastassia2312 <23122000na@gmail.com>
2023-10-19 08:23:35 +0300

    autocheck

---
+++ java.js
@@ -0,0 +1,27 @@
+/*function getFileName(file) {
+  return file.indexOf(".") > -1 ? file.slice(0, file.indexOf(".")) : file;
+}
+
+console.log(getFileName("index.html"));
+console.log(getFileName("styles"));*/
+
+/*function calculateTotal(number) {
+  let counter;
+  let sum;
+  while (sum > number) {
+    counter = number + 1;
+    sum = counter + number;
+    return sum;
+  }
+}
+console.log(calculateTotal(3));
+*/
+function calculateEvenTotal(number) {
+  let sum = 0;
+  for (let counter = 0; counter <= number; counter += 1) {
+    counter++;
+    sum = sum + number;
+  }
+  return sum;
+}
+console.log(calculateEvenTotal(6));
